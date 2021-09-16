e90db286cf6d97f265a32d07bff94f8ce1e4d1f1
Aymn S.Al-Arrak <jucko_15@yahoo.com>
2021-09-16 21:24:58 +0300

    create data classes

---
+++ app/src/main/java/com/example/triviagame/data/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.triviagametask.data
+
+object Constants {
+    const val POINTS = "points"
+    const val WIN_TEXT = "Congratulations!! you did it "
+    const val LOSE_TEXT = " You fail , try Again "
+
+}
---
+++ app/src/main/java/com/example/triviagame/data/TriviaQuestion.kt
@@ -0,0 +1,6 @@
+package com.example.triviagametask.data
+
+data class TriviaQuestion(
+    val response_code: Int?,
+    val results: List<Result>?
+)
